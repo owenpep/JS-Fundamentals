603218936ea7f8d9ab27ce1d676fa13fbe3b10cd
owenpep <aigbeowenp@gmail.com>
2025-05-24 23:15:07 -0400

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const count = parseInt(process.argv[2]);
 
-if (isNaN(count) || count < 0) {
+if (isNaN(count)) {
   console.log("Missing number of occurrences");
 } else {
   for (let i = 0; i < count; i++) {
